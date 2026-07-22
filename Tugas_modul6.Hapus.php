448e911c56c5b24b5fc398d369da8a436884a72d
satriyani290305-hash <satriyani290305@gmail.com>
2026-07-22 21:32:15 +0800

    Update Tugas_modul6.Hapus.php

--- Tugas_modul6.Hapus.php
+++ Tugas_modul6.Hapus.php
@@ -1,1 +1,22 @@
+<?php
+include 'koneksi.php';
 
+if (isset($_GET['id'])) {
+
+    $id = $_GET['id'];
+    
+    $sql = "DELETE FROM produk WHERE id_produk='$id'";
+
+    if ($conn->query($sql) === TRUE) {
+        header("Location: index.php");
+        exit();
+    } else {
+        echo "Error: " . $conn->error;
+    }
+
+} else {
+    echo "ID tidak ditemukan!";
+}
+
+$conn->close();
+?>
